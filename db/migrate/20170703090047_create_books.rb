b2925a981fdaab4842862623ecbf05e7843c99cc
Henry KIM <seedtree.henry@gmail.com>
2017-07-03 19:23:32 +1000

    bookstore

---
+++ db/migrate/20170703090047_create_books.rb
@@ -0,0 +1,11 @@
+class CreateBooks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :books do |t|
+      t.string :author
+      t.string :title
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
